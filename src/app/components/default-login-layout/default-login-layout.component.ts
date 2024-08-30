1c9a9e433056907b325239af6d704f282165e75f
Guimti <guilhermeguimti@gmail.com>
2024-08-30 00:18:36 -0300

    finalizado criação dos botões

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -9,6 +9,9 @@ import { Component, Input } from '@angular/core';
 })
 export class DefaultLoginLayoutComponent {
 @Input() title: string = "";
+@Input() primaryBtnText: string = "";
+@Input() secpondaryBtnText: string = "";
+
 
 
 }
